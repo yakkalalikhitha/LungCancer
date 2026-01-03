f21fbc1ee4bbea276275db6ab3983e72af26a510
yakkalalikhitha <likhithayakkala@gmail.com>
2026-01-03 10:35:23 +0530

    SecondCommit-3126

--- notebook/app.py
+++ notebook/app.py
@@ -84,4 +84,7 @@ def predict():
 # Run App
 # -----------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
